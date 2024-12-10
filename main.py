ba46d361c952e3ac8765fb9674300793cb8a5d67
leamolina <120037925+leamolina@users.noreply.github.com>
2024-12-10 23:59:28 +0100

    importation du jeu

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("Coucou")
+import gymnasium as gym
+import ale_py
+
+gym.register_envs(ale_py)
+env = gym.make("ALE/Assault-v5", render_mode="human")
+obs, info = env.reset()
+
+done = False
+while not done:
+    action = env.action_space.sample()  # Random action
+    obs, reward, terminated, truncated, info = env.step(action)
+    done = terminated or truncated
+
+env.close()
+
+
+print("Observation shape:", obs.shape)
+print("Action space:", env.action_space)
